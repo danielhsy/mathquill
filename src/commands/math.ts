55277f28303db726f37a4c9f9b0538fd58a4bf09
root <root@www.imathas.com>
2026-03-26 06:12:58 +0000

    A first stab at readding layout calcs to supsub and nthroot

--- src/commands/math.ts
+++ src/commands/math.ts
@@ -75,6 +75,13 @@ class MathElement extends MQNode {
       });
     }
   }
+  reflow() {
+    // if element reflows, and right is supsub, that needs to reflow too
+    // to recalculate layout
+    if (this[R] instanceof SupSub) {
+      this[R].reflow();
+    }
+  }
 }
 
 class DOMView {
